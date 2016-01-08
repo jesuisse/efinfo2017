25d76c40583fc9da400bc85524c67463c84d4fef
shu <pascal.schuppli@gymbiel-seeland.ch>
2016-01-08 15:24:58 +0100

    Tests korrigiert

--- chainreaction_unittest/chainreaction_test.py
+++ chainreaction_unittest/chainreaction_test.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from chainreaction1 import *
+from chainreaction5 import *
 
 
 class Chainreaction_1(unittest.TestCase):
     #def test_macheTeilchen(self):
-        #self.assertIsNotNone(macheTeilchen(), [pos, farbe, v])
+    #self.assertIsNotNone(macheTeilchen(), [pos, farbe, v])
 
-   def test_normalisiere(self):
-       self.assertEqual(normalisiere(vektor), 1)
-   def testnormalisiere2(self):
-        self.assertIs(laenge, 1)
+    def test_normalisiere(self):
+        vektor = [4, 5]
+        n = normalisiere(vektor)
+        self.assertEqual(n[0]**2 + n[1]** 2, 1)
+
 
     def testmacheTeilchen(self):
-        self.assertIn(macheTeilchen(), Teilchen)
+        t = macheTeilchen()
+        self.assertEqual(type(t), list)
+        self.assertEqual(len(t), 3)
 
     def testmacheTeilchen2(self):
         self.assertIsNotNone(macheTeilchen(), Teilchen)
 
     def testmacheTeilchen3(self):
-        self.assertEqual(macheTeilchen(), v == float)
+        t = macheTeilchen()
+        self.assertEqual(type(t[2]), list)
+        self.assertEqual(type(t[2][0]), float)
+        self.assertEqual(type(t[2][1]), float)
 
 
 
@@ -31,5 +37,3 @@ class Chainreaction_1(unittest.TestCase):
 
 
 
-
-if __name__ == "__main__":
