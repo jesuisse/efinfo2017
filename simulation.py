22764545e06887803119675d7a175cad5c8cd058
fridave <david@stadtgut.ch>
2016-01-10 16:12:39 +0100

    simulation

--- simulation.py
+++ simulation.py
@@ -11,7 +11,7 @@ Area = [10,10]
 
 maxfood = Area[0]*Area[1]
 
-food = 40
+food = 70
 
 growth = 0.01
 
@@ -25,6 +25,18 @@ class Herbivore():
         self.children = None
         self.age = 0
 
+    def reproduce(self):
+        n = 0
+        for i in population:
+            n += 1
+        parent = self
+        population.append(n)
+        creature.append(n)
+        creature[n] = Herbivore()
+        creature[n]._init_()
+        creature[n].parent = parent
+        creature[n].health = 3
+
     def live(self):
 
         if self.health == 0:
@@ -37,9 +49,13 @@ class Herbivore():
                     self.health = self.health + 1
                     global food
                     food = food - 1
+        if self.health > 5 and self.age > 5:
+            self.reproduce()
+            #self.health -= 1
+            self.fitness += 1
+        self.age += 1
 
-    def reproduce(self):
-        pass
+
 
 t = 0
 end = 50
@@ -78,9 +94,9 @@ while t < end:
     print "\nFood:",food,"\n"
     for i in population:
         if  creature[i].alive == True:
-            print "Creature",i,"    Health:",creature[i].health
+            print "Creature",i,"    Fitness:",creature[i].fitness,"    Health:",creature[i].health
         else:
-            print "Creature",i,"    Dead"
+            print "Creature",i,"    Fitness:",creature[i].fitness,"    Dead"
 
 
 
